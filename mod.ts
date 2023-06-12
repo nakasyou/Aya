35ac2988db789aa2d6bea15f2af52bfa91efb11f
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-06-12 17:05:48 +0900

    Update export type

--- mod.ts
+++ mod.ts
@@ -1,2 +1,5 @@
+export * from "./server/mod.ts"
 export * as server from "./server/mod.ts"
-export * as client from "./client/mod.ts"
+
+export * from "./client/mod.ts"
+export * as server from "./client/mod.ts"
